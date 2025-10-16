6c32ee43063e85e4d31cca5b78ef626543781d5e
mirnaSafar <105860511+mirnaSafar@users.noreply.github.com>
2025-10-16 13:55:14 +0300

    Update app_config.dart

--- lib/src/core/app/app_config/app_config.dart
+++ lib/src/core/app/app_config/app_config.dart
@@ -2,5 +2,5 @@ class AppConfig {
   static bool isLocationRequired = true;
   static String defaultLanguage = 'ar';
   static bool subscriptionStatus = true;
-  static String google_api_key = "AIzaSyCq7hlabnqxQyrP2xPUlRMycNZR1eWQZmc";
+  static String google_api_key = "*****yCq7hlabnqxQyrP2xPUlRMycNZR1eW****";
 }
